0e50b1ec92840bedc36326308ffa6e0511a81a3e
Anmol <kr.anmol27@gmail.com>
2025-10-06 20:59:02 +0100

    website upload

---
+++ loadComponents.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => 
+{
+  fetch('footer.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('footer-placeholder').innerHTML = data;
+    });
+});
